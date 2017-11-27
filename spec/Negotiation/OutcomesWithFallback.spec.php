df491e5267a7a21f4dab8e548bbf628c2d950e07
pmall <pierre.mallinjoud@gmail.com>
2017-11-27 22:19:28 +0100

    add the outcomes with fallback class

---
+++ spec/Negotiation/OutcomesWithFallback.spec.php
@@ -0,0 +1,74 @@
+<?php
+
+use function Eloquent\Phony\Kahlan\stub;
+use function Eloquent\Phony\Kahlan\mock;
+
+use Psr\Http\Message\ServerRequestInterface;
+
+use Ellipse\Negotiation\Agreement;
+use Ellipse\Negotiation\NegotiatorAdapter;
+use Ellipse\Negotiation\OutcomesWithFallback;
+use Ellipse\Negotiation\OutcomesInterface;
+use Ellipse\Negotiation\Exceptions\NegotiationFailedException;
+
+describe('OutcomesWithFallback', function () {
+
+    beforeEach(function () {
+
+        $this->key = 'fallback';
+        $this->factory = stub();
+        $this->delegate = mock(OutcomesInterface::class);
+
+        $this->outcomes = new OutcomesWithFallback($this->key, $this->factory, $this->delegate->get());
+
+    });
+
+    it('should implement OutcomesInterface', function () {
+
+        expect($this->outcomes)->toBeAnInstanceOf(OutcomesInterface::class);
+
+    });
+
+    describe('->agreement()', function () {
+
+        beforeEach(function () {
+
+            $this->negotiator = mock(NegotiatorAdapter::class);
+
+        });
+
+        context('when the delegate ->agreement() method throws a NegotiationFailedException', function () {
+
+            it('should return a new Agreement', function () {
+
+                $exception = mock(NegotiationFailedException::class)->get();
+
+                $this->delegate->agreement->with($this->negotiator, 'text/html')->throws($exception);
+
+                $test = $this->outcomes->agreement($this->negotiator->get(), 'text/html');
+
+                expect($test)->toBeAnInstanceOf(Agreement::class);
+
+            });
+
+        });
+
+        context('when the delegate ->agreement() method do not throw a NegotiationFailedException', function () {
+
+            it('should proxy the delegate ->agreement() method', function () {
+
+                $agreement = mock(Agreement::class)->get();
+
+                $this->delegate->agreement->with($this->negotiator, 'text/html')->returns($agreement);
+
+                $test = $this->outcomes->agreement($this->negotiator->get(), 'text/html');
+
+                expect($test)->toBe($agreement);
+
+            });
+
+        });
+
+    });
+
+});
--- src/Negotiation/OutcomesWithFallback.php
+++ src/Negotiation/OutcomesWithFallback.php
@@ -8,19 +8,50 @@ use Ellipse\Negotiation\Exceptions\NegotiationFailedException;
 
 class OutcomesWithFallback implements OutcomesInterface
 {
+    /**
+     * The fallback key.
+     *
+     * @var string
+     */
     private $key;
 
+    /**
+     * The fallback factory.
+     *
+     * @var callable
+     */
     private $factory;
 
+    /**
+     * The delegate.
+     *
+     * @var \Ellipse\Negotiation\OutcomesInterface
+     */
     private $delegate;
 
-    public function __construct(string $key, Agreement $factory, OutcomesInterface $delegate)
+    /**
+     * Set up a outcomes with fallback using the given key, factory and
+     * delegate.
+     *
+     * @param string                                    $key
+     * @param callable                                  $factory
+     * @param \Ellipse\Negotiation\OutcomesInterface    $delegate
+     */
+    public function __construct(string $key, callable $factory, OutcomesInterface $delegate)
     {
         $this->key = $key;
         $this->factory = $factory;
         $this->delegate = $delegate;
     }
 
+    /**
+     * Return an agreement using the fallback key and factory when the delegate
+     * fails to find an agreement.
+     *
+     * @param \Ellipse\Negotiation\NegotiatorAdapter    $negotiator
+     * @param string                                    $accept
+     * @return \Ellipse\Negotiation\Agreement
+     */
     public function agreement(NegotiatorAdapter $negotiator, string $accept): Agreement
     {
         try {
@@ -31,7 +62,7 @@ class OutcomesWithFallback implements OutcomesInterface
 
         catch (NegotiationFailedException $e) {
 
-            return new Agreement($key, $this->factory);
+            return new Agreement($this->key, $this->factory);
 
         }
     }
